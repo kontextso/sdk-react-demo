b112772d052bd4e04813fb5ddd8cbae89319c59e
JakubMrozek <jakub.mrozek@gmail.com>
2025-07-23 19:39:27 +0200

    init demo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,112 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import { AdsProvider, InlineAd } from "@kontextso/sdk-react";
+import { useState } from "react";
+import { PUBLISHER_TOKEN, PLACEMENT_CODE } from "./constants";
+
+interface Message {
+  id: string;
+  role: string;
+  content: string;
+  createdAt: Date;
+}
+
+const getRandomId = () => {
+  return Date.now() + Math.random().toString(36).substring(2, 15);
+}
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [theme, setTheme] = useState<"light" | "dark">("light");
+  const [isLoading, setIsLoading] = useState(false);
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const [conversationId] = useState(() => getRandomId());
+  const [userId] = useState(() => getRandomId());
+
+  const [messages, setMessages] = useState<Message[]>(() => [
+    {
+      id: getRandomId(),
+      role: "user",
+      content: "Hello, how are you?",
+      createdAt: new Date()
+    },
+    {
+      id: getRandomId(),
+      role: "assistant",
+      content: "I'm good, thank you!",
+      createdAt: new Date(new Date().getTime() + 1000)
+    }
+  ]);
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const message = formData.get("message") as string;
+    (e.target as HTMLFormElement).reset();
+
+    setMessages((prevMessages) => [...prevMessages, {
+      id: getRandomId(),
+      role: "user",
+      content: message,
+      createdAt: new Date()
+    }]);
+
+    // simulate loading chat response with the assistant role
+    setIsLoading(true);
+
+    setTimeout(() => {
+      setIsLoading(false);
+
+      setMessages((prevMessages) => [...prevMessages, {
+        id: getRandomId(),
+        role: "assistant",
+        content: "This is a test response from the assistant.",
+        createdAt: new Date()
+      }]);
+    }, 1000);
+  }
+  
+  return (
+    <div className="container" data-theme={theme}>
+      <div className="page">
+        <header>
+          <button onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
+            Switch to {theme === "light" ? "Dark" : "Light"} theme
+          </button>
+        </header>
+        <main>
+          <AdsProvider
+            messages={messages}
+            publisherToken={PUBLISHER_TOKEN}
+            userId={userId}
+            conversationId={conversationId}
+            enabledPlacementCodes={[PLACEMENT_CODE]}
           >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            <div>
+              {messages.map((message) => (
+                <div key={message.id}>
+                  <p><strong>{message.role}</strong>: </p>
+                  <p>{message.content}</p>
+
+                  <InlineAd
+                    code={PLACEMENT_CODE}
+                    messageId={message.id}
+                  />
+                </div>
+              ))}
+
+              {isLoading && <p>Loading...</p>}
+          </div>
+          </AdsProvider>
+        </main>
+        <footer>
+          <form onSubmit={onSubmit}>
+            <textarea name="message" disabled={isLoading} required rows={5} />
+            <div>
+              <button type="submit" disabled={isLoading}>Send</button>
+            </div>
+          </form>
+        </footer>
+      </div>
     </div>
   );
 }
